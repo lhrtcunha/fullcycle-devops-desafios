bd1b931adda57ff864b9a6a635a1df519e8e11c9
Luis H <luishrtcunha@yahoo.com.br>
2022-02-11 06:35:10 -0300

    Desafio docker nginx-node

--- Docker/node/node/index.js
+++ Docker/node/node/index.js
@@ -7,16 +7,21 @@ const config = {
     password: 'root',
     database:'nodedb'
 };
-/*const mysql = require('mysql')
+const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
 const sql = `INSERT INTO people(name) values('Wesley')`
 connection.query(sql)
-connection.end()*/
+connection.end()    
+
+connection.query('SELECT name FROM people', (err,rows) => {
+    if(err) throw err;
+    rasp = '<h1>Full Cycle Rocks!</h1>' + rows;
+});
 
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+    res.send(rasp)
 })
 
 app.listen(port, ()=> {
